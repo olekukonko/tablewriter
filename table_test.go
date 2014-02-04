3dc27da4dd752f04df2f0b16feb14604ccd55b71
olekukonko <github.com@12>
2014-02-04 14:52:19 +0100

    Added more example

--- table_test.go
+++ table_test.go
@@ -5,26 +5,17 @@ import (
 	"testing"
 )
 
-func TestLong(t *testing.T) {
+func ExampleShort(t *testing.T) {
 
 	data := [][]string{
-		[]string{"Learn East has computers with adapted keyboards with enlarged print etc. There is also a portable loop system and a minicom", "  y  ", " y "},
-		[]string{"Instead of lining up the letters all the way across, he splits the keyboard in two", "Like most ergonomic keyboards", "x"},
+		[]string{"A", "The Good", "500"},
+		[]string{"B", "The Very very Bad Man", "288"},
+		[]string{"C", " The Ugly ", "120"},
+		[]string{"D", "The Gopher  Cam to lagos on the first day of chirstmas alone here", "800"},
 	}
-
-
-		data = [][]string{
-			[]string{"The Good man", "B", "500"},
-			[]string{"D", "The Very very Bad Man", "288"},
-			[]string{"D", " E ", "288"},
-		}
-
-
 
 	table := NewTable(os.Stdout)
 	table.SetHeader([]string{"Name", "Sign", "Rating"})
-	table.SetCenterSeparator("*")
-	table.SetRowSeparator("=");
 
 	for _, v := range data {
 		table.Append(v)
@@ -32,3 +23,23 @@ func TestLong(t *testing.T) {
 	table.Render()
 
 }
+
+func ExampleLong(t *testing.T) {
+
+	data := [][]string{
+		[]string{"Learn East has computers with adapted keyboards with enlarged print etc", "  Some Data  ", " Another Data"},
+		[]string{"Instead of lining up the letters all ", "the way across, he splits the keyboard in two", "Like most ergonomic keyboards", "See Data"},
+	}
+
+	table := NewTable(os.Stdout)
+	table.SetHeader([]string{"Name", "Sign", "Rating"})
+	table.SetCenterSeparator("*")
+	table.SetRowSeparator("=")
+
+	for _, v := range data {
+		table.Append(v)
+	}
+	table.Render()
+
+}
+
