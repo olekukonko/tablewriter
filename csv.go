64e840a8ea431e72c37ea2f91604f73f2f2fdf72
Michael Vogt <mvo@ubuntu.com>
2014-12-16 14:29:43 +0100

    make "table" a exported type "Table" to allow extending it
    The current table type makes it hard to extend the table. If I want
    to create a table with a different default appearance thats hard,
    i.e. I can't created "func NewCustomTable() *table {}". By exporting
    the type this is now possible.
    
    The commit also adds a test for this new NewCustomizedTable().
    Some whitespace changes because "go fmt" was also run.

--- csv.go
+++ csv.go
@@ -15,10 +15,10 @@ import (
 
 // Start A new table by importing from a CSV file
 // Takes io.Writer and csv File name
-func NewCSV(writer io.Writer, fileName string, hasHeader bool) (*table, error) {
+func NewCSV(writer io.Writer, fileName string, hasHeader bool) (*Table, error) {
 	file, err := os.Open(fileName)
 	if err != nil {
-		return &table{}, err
+		return &Table{}, err
 	}
 	defer file.Close()
 	csvReader := csv.NewReader(file)
@@ -29,13 +29,13 @@ func NewCSV(writer io.Writer, fileName string, hasHeader bool) (*table, error) {
 //  Start a New Table Writer with csv.Reader
 // This enables customisation such as reader.Comma = ';'
 // See http://golang.org/src/pkg/encoding/csv/reader.go?s=3213:3671#L94
-func NewCSVReader(writer io.Writer, csvReader *csv.Reader, hasHeader bool) (*table, error) {
+func NewCSVReader(writer io.Writer, csvReader *csv.Reader, hasHeader bool) (*Table, error) {
 	t := NewWriter(writer)
 	if hasHeader {
 		// Read the first row
 		headers, err := csvReader.Read()
 		if err != nil {
-			return &table{}, err
+			return &Table{}, err
 		}
 		t.SetHeader(headers)
 	}
@@ -44,7 +44,7 @@ func NewCSVReader(writer io.Writer, csvReader *csv.Reader, hasHeader bool) (*tab
 		if err == io.EOF {
 			break
 		} else if err != nil {
-			return &table{}, err
+			return &Table{}, err
 		}
 		t.Append(record)
 	}
